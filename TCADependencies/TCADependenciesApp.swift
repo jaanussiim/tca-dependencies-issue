1013ea8dd1a90e9d48320f520f0d7944dcbf48f0
Jaanus Siim <jaanus@jaanussiim.com>
2023-01-10 08:22:44 +0200

    first commit

---
+++ TCADependencies/TCADependenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADependenciesApp.swift
+//  TCADependencies
+//
+//  Created by Jaanus Siim on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TCADependenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
